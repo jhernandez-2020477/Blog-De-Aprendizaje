180b48281d22cfd58e24c8bedba47f52c02b8a0e
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-05-06 16:26:42 -0600

    Se agrega modelo de Publicacion y se renombrán archivos

---
+++ Blog-Backend/src/publication/publication.model.js
@@ -0,0 +1,27 @@
+//Modelo de Publicación
+const publicationSchema = Schema(
+    {
+        title: {
+            type: String,
+            maxLength: [50, `Can´t be overcome 50 characters`],
+            required: [true, 'Title is required']
+        },
+        description: {
+            type: String,
+            maxLength: [100, `Can´t be overcome 100 characters`],
+            required: [true, 'Content is required']
+        },
+        course: {
+            type: String,
+            maxLength: [15, `Can´t be overcome 15 characters`],
+            required: [true, 'Course is required']
+        },
+        date: { 
+            type: Date, 
+            default: Date.now 
+        }
+    }
+)
+
+//Crear y exportar el modelo
+export default model('Publication', publicationSchema)
