b273b08003e9041a413441c91b14085add272f12
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-05-06 17:46:15 -0600

    Se agregan primeras validaciones, validacones de Publicacion

---
+++ Blog-Backend/helpers/validators.js
@@ -0,0 +1,35 @@
+import { body } from "express-validator"; //Capturar todo el body de la solicitud
+import { validateErrorWithoutImg } from "./validate.error.js";
+
+/*----------------------------- PUBLICACIÓN --------------------------------- */
+export const validSavePost = [
+    body('title', 'Title cannot be empty')
+        .notEmpty() 
+        .isLength({ max: 50 })
+        .withMessage('Can´t be overcome 50 characters'),
+    body('description', 'Content cannot be empty')
+        .notEmpty()
+        .isLength({ max: 100 })
+        .withMessage('Can´t be overcome 50 characters'), 
+    body('course', 'Course cannot be empty')
+        .notEmpty()
+        .isLength({ max: 15 })
+        .withMessage('Can´t be overcome 15 characters'),      
+        validateErrorWithoutImg
+]
+
+export const validUpdatePost = [
+    body('title', 'Title is optional')
+        .optional() 
+        .isLength({ max: 50 })
+        .withMessage('Can´t be overcome 50 characters'),
+    body('content', 'Content is optional')
+        .optional()
+        .isLength({ max: 100 })
+        .withMessage('Can´t be overcome 50 characters'), 
+    body('course', 'Course cannot be empty')
+        .optional()
+        .isLength({ max: 15 })
+        .withMessage('Can´t be overcome 15 characters'),       
+        validateErrorWithoutImg
+]
