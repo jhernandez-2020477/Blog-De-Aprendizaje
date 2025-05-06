e41d39e421e3d36d81375143a21dacd360eb2635
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-05-05 20:15:59 -0600

    Se agregan bases del Backend del Blog de aprendizaje

---
+++ Blog-Backend/configs/app.js
@@ -0,0 +1,34 @@
+//Configuración del Servidor de express
+
+'use strict'
+
+//ECModules
+import express from "express" //Servidor HTTP
+import morgan from "morgan" //Logs
+import helmet from "helmet" //Seguridad para HTTP
+import cors from 'cors' //Acceso al API
+
+
+const configs = (app )=>{
+    app.use(express.json()) //Aceptar y enviar datos JSON
+    app.use(express.urlencoded({extended: false})) //No encriptar
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan('dev'))
+}
+
+const routes = (app)=>{
+    app.use()
+}
+
+export const initServer = async()=>{
+    const app = express()
+    try {
+        configs(app)
+        routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Server running in port ${process.env.PORT}`)
+    } catch (err) {
+        console.error('Server init failed', err)
+    }
+}
