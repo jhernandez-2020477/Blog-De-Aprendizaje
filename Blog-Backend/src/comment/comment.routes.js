c8608a301a115c9b65219a41528b512d67a8005c
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-05-06 19:11:05 -0600

    Se agregan rutas del controlador en Comentario

---
+++ Blog-Backend/src/comment/comment.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { deleteComment, getAllComments, saveComment, updateComment } from "./comment.controller.js";
+import { validSaveComment, validUpdateComment } from "../../helpers/validators.js";
+
+const api = Router()
+
+//Rutas públicas
+api.post('/save', [validSaveComment], saveComment)
+api.put('/:id', [validUpdateComment], updateComment)
+api.delete('/:id', deleteComment)
+api.get('/getAll', getAllComments)
+
+//Exportar
+export default api
