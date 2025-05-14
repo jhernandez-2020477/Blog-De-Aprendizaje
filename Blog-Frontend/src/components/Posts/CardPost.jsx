c711023956e7be3dc481092270212b7baff475a5
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-05-13 21:48:10 -0600

    Se modifica estructura de PostCard

--- Blog-Frontend/src/components/Posts/CardPost.jsx
+++ Blog-Frontend/src/components/Posts/CardPost.jsx
@@ -1,14 +1,15 @@
-export const CardPost = ({title, description, course, date}) => {
-    return (
-        <div className="card" style={{width: '18rem'}}>
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <h6 className="card-subtitle mb-2 text-body-secondary">{description}</h6>
-                <p className="card-text">{course}</p>
-                <p className="card-text">{date}</p>
-                <a href="#" className="card-link">Card link</a>
-                <a href="#" className="card-link">Another link</a>
-            </div>
-        </div>
-    )
-}
+import { Link } from "react-router-dom";
+
+export const CardPost = ({ _id, title, description, course, date }) => {
+  return (
+    <div className="card m-2 p-3 shadow w-100">
+      <h5>{title}</h5>
+      <p>{description}</p>
+      <p>{course}</p>
+      <p className="text-muted">{new Date(date).toLocaleDateString()}</p>
+      <Link to={`/feed/post/${_id}`} className="btn btn-outline-primary">
+        Ver comentarios
+      </Link>
+    </div>
+  );
+};
